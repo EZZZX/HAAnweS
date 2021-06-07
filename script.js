3bccad1309f16f607cb37362c21b60797720bb36
eduardChalovski <85243017+eduardChalovski@users.noreply.github.com>
2021-06-07 19:29:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 $( document ).ready(function() {
-  let mtoken = "X-Group-Token: bEdoZZXsDIlm";
+  let mtoken = "bEdoZZXsDIlm";
   
   $("h1").click(function() {
         $.ajax({
             type: "GET",
             url: "http://18.197.21.42:8080/channels/1",
-            //твоё предложение скинуть стринг... сейчас дошло, что может он рассматривает мтокен как объект, мб стоит закастить на стринг
-            mtoken,
-
-            /*до этого здесь стояло:
             headers:{
-              Authentication : mtoken
-            }, */
-          
-            success: function(data, status) {
-                alert(status);
+                "X-Group-Token": mtoken
             },
+            contentType: "applicatoin/json",
+            success: function(data, status) {alert(status);},
             error: function() {
                 alert("error yay")
             }
