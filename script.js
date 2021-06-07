23160eda5ad17a309dd3c1f57d0bdf310d94e0f6
eduardChalovski <85243017+eduardChalovski@users.noreply.github.com>
2021-06-07 19:50:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 $( document ).ready(function() {
-  let mtoken = "bEdoZZXsDIlm";
-  
-  $("h1").click(function() {
+    let mtoken =  "bEdoZZXsDIlm";
+    let chanelList;
+
+    $("h1").click(function() {
         $.ajax({
             type: "GET",
-            url: "http://18.197.21.42:8080/channels/1",
+            url: "http://18.197.21.42:8080/channels/",
             headers:{
                 "X-Group-Token": mtoken
             },
             contentType: "applicatoin/json",
-            success: function(data, status) {alert(status);},
+            success: function(content, status) {
+                chanelList=JSON.parse(content);
+                alert(chanelList[0].id)    
+            },
             error: function() {
                 alert("error yay")
             }
         });
     });
-
 });
